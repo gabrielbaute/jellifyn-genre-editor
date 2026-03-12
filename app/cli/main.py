bc624274809cb6dc90486c93f4070f75205d4062
gabrielbaute <gabrielbaute@gmail.com>
2026-03-11 20:07:11 -0400

    Entrypoint que orquesta todos los comandos de la CLI

---
+++ app/cli/main.py
@@ -0,0 +1,52 @@
+import sys
+from rich.console import Console
+from argparse import ArgumentParser, Namespace
+
+from app.services.jellyfin_client_service import JellyfinClientService
+from app.settings.app_settings import settings
+from app.settings.app_logs_settings import JGELogger
+
+from app.cli.parser import create_parser
+from app.cli.commands import show_version, initialize_config, analyze_artist, analyze_album, analyze_track
+
+JGELogger.setup_logging()
+
+def main() -> None:
+    parser: ArgumentParser = create_parser(settings=settings)
+    args: Namespace = parser.parse_args()
+
+    console = Console()
+
+    if args.command == "version":
+        show_version(console, settings)
+    
+    elif args.command == "init":
+        initialize_config(
+            console=console, 
+            settings=settings, 
+            host=args.host, 
+            api_key=args.api_key, 
+            app_name=args.app_name
+        )
+
+    elif args.command == "analyze":
+        jf_service = JellyfinClientService(settings)
+        try:
+            if not jf_service.verify_connection():
+                return
+
+            if args.track:
+                analyze_track(console, jf_service, args.track)
+            elif args.album:
+                analyze_album(console, jf_service, args.album)
+            elif args.artist:
+                # Si solo pones artista, mostramos el artista
+                analyze_artist(console, jf_service, args.artist)
+                # Opcional: Podríamos llamar automáticamente a analyze_albums del artista aquí
+            else:
+                console.print("[yellow]Debes especificar --artist, --album o --track[/yellow]")
+        finally:
+            jf_service.close()
+
+if __name__ == "__main__":
+    main()
